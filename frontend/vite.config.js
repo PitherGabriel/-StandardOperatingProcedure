6602a2daa4ce4e15ececdb8a18690907867a4b86
PitherGabriel <pitherga2001@gmail.com>
2026-02-04 20:34:50 +0100

    changed docker compose for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-    host: true,
-    proxy: {
-      "/api": {
-        target: "http://pos-backend:5000",
-        changeOrigin: true,
-      },
-    },
-  },
+  build: {
+    outDir: 'dist'
+  }
 
 })
