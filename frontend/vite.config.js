2fa7ce37d91af0eb309d6c7e5a4972f27be78d98
PitherGabriel <pitherga2001@gmail.com>
2026-04-21 09:58:44 +0200

    FIxed env variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_BACKEND_API_URL': JSON.stringify(process.env.VITE_BACKEND_API_URL)
+    'import.meta.env.VITE_BACKEND_API_URL': JSON.stringify(
+      process.env.VITE_BACKEND_API_URL || '/api'  // fallback to /api
+    )
   },
   build: {
     outDir: 'dist'
   },
   server: {
-    host: '0.0.0.0',  // allows connections from outside the container
+    host: '0.0.0.0',
     port: 5173,
   }
 })
