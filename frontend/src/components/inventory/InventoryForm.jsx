218e2d2e2063000e41797a669e1c3d72f1ff4b7d
PitherGabriel <pitherga2001@gmail.com>
2026-05-08 17:29:55 +0200

    deleted ttitle for agregatting products

--- frontend/src/components/inventory/InventoryForm.jsx
+++ frontend/src/components/inventory/InventoryForm.jsx
@@ -77,7 +77,6 @@ export default function InventoryForm({ onAdded, showNotification }) {
   return (
     <div className="max-w-3xl mx-auto">
       <div className="bg-white rounded-lg shadow-lg p-6">
-        <h2 className="text-2xl font-bold text-gray-800 mb-6">Agregar Nuevo Producto</h2>
         <div className="space-y-4">
 
           <div>
